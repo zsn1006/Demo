e11fe70cbedae80f1d3ac98254fffebbec8c3bfb
zsn1006 <zsn1006@163.com>
2016-07-04 14:50:13 +0800

    all broadcastreceiver

--- app/src/main/java/com/polycom/polycom/broadcastReciver/BroadCastActivity.java
+++ app/src/main/java/com/polycom/polycom/broadcastReciver/BroadCastActivity.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,35 +20,52 @@ import com.polycom.polycom.R;
  */
 public class BroadCastActivity extends Activity {
     private NetworkChangeReceiver networkChangeReceiver;
+    //本地广播
+    private LocalBroadcastManager localBroadcastManager;
+    private LocalReceiver localReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_broadcast);
+        localBroadcastManager=LocalBroadcastManager.getInstance(this);
+        //系统网络广播
         IntentFilter intentFilter=new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver=new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
         //test git commit
         //..................................................
-<<<<<<< HEAD
+        //本地广播
+        IntentFilter localIntentFilter=new IntentFilter();
+        localIntentFilter.addAction("com.polycom.polycom.LOCAL_BROADCAST");
+        localReceiver=new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, localIntentFilter);
         Button sendBroadcast = (Button) findViewById(R.id.send_broadcast);
         //自定义广播
         sendBroadcast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.polycom.polycom.MY_BROADCAST");
-                sendBroadcast(intent);
+                //Intent intent = new Intent("com.polycom.polycom.MY_BROADCAST");
+                //com.polycom.polycom.LOCAL_BROADCAST
+                Intent intent = new Intent("com.polycom.polycom.LOCAL_BROADCAST");
+                intent.putExtra("data", "15311480854");
+                //发送无序的广播
+                //sendBroadcast(intent);
+                //发送有序的广播
+               // sendOrderedBroadcast(intent,null);
+                //发送本地的广播
+                localBroadcastManager.sendBroadcast(intent);
+                //sendBroadcast(intent);
             }
         });
-=======
->>>>>>> master
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
     class NetworkChangeReceiver extends BroadcastReceiver{
